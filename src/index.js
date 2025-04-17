77e175dc183faaaec31302de55239fdb7eff9783
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2025-04-17 18:41:47 -0400

    JS

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function makePoem(event) {
 
 	let apiKey = "006e1d3aa42ft5cc55o041daac8db188";
 	let context =
-		"you are an inspirational poet who loves to write. Your instructions are to create a four-line poem about Studio Ghibli movies and characters in basic HTML. Please follow the user instructions";
+		"you are an inspirational poet who loves to write. Your instructions are to create a four-line poem about Studio Ghibli movies in basic HTML. Do not add a title. Please follow the user instructions";
 	let prompt = `User instructions: Create a poem inspired by ${instructionsInput.value}`;
 
 	let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
