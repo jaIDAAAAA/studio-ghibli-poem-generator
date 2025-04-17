b36f64092cac76e7cfa83e531767c70260186a2b
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2025-04-17 13:48:16 -0400

    basic JS form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function makePoem(event) {
+	event.preventDefault();
 
+	new Typewriter("#poem", {
+		strings: ["With the anger of San..."],
+		autoStart: true,
+		delay: 10,
+		cursor: "",
+	});
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", makePoem);
