d82a57311636aa536ee5d6b0a277c0a3a763eb46
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2025-04-17 16:18:15 -0400

    clean up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function makePoem(event) {
-	event.preventDefault();
-
+function showPoem(response) {
 	new Typewriter("#poem", {
-		strings: "With the anger of San...",
+		strings: response.data.answer,
 		autoStart: true,
 		delay: 10,
 		cursor: "",
 	});
+}
+
+function makePoem(event) {
+	event.preventDefault();
+	let instructionsInput = document.querySelector("#user-instructions");
+
+	let apiKey = "006e1d3aa42ft5cc55o041daac8db188";
+	let context =
+		"you are an inspirational poet who loves to write. Your instructions are to create a four-line poem about Studio Ghibli movies and characters in basic HTML. Please follow the user instructions";
+	let prompt = `User instructions: Create a poem inspired by ${instructionsInput.value}`;
+
+	let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+
+	console.log("Making poem");
+	console.log(`Prompt: ${prompt}`);
+	console.log(`Context: ${context}`);
+	axios.get(apiUrl).then(showPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
